afbba9fb84f2cbe97d5a4c20e246333f9e0eb0a7
Carlos Daniel de Souza Cordeiro <134225062+DanielSouzzz@users.noreply.github.com>
2024-03-27 22:22:08 -0300

    Desenvolvi a classe Integration.

--- src/main/java/org/example/Integration.java
+++ src/main/java/org/example/Integration.java
@@ -1,5 +1,33 @@
 package org.example;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.services.calendar.Calendar;
+import com.google.api.services.calendar.CalendarScopes;
+
+import java.io.IOException;
+import java.util.Collections;
+
 public class Integration {
-    // buscar hoŕarios livres da api daqui
+    private static final String APP_NAME = "MyBot";
+
+    private Calendar service;
+
+    public Integration() throws IOException {
+        // Autenticação
+        GoogleCredential credential = GoogleCredential
+                .getApplicationDefault()
+                .createScoped(Collections.singleton(CalendarScopes.CALENDAR));
+
+        // Inicia o serviço do Google Calendar.
+        service = new Calendar.Builder(
+                credential.getTransport(),
+                credential.getJsonFactory(),
+                credential)
+                .setApplicationName(APP_NAME)
+                .build();
+    }
+
+    public Calendar getService() {
+        return service;
+    }
 }
